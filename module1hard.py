c154bbbff0347e9fa10401067f1f60998171e2f9
AnMarMag569 <m.gordeeva63@gmail.com>
2024-06-15 22:59:51 +0200

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,22 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+print(grades)
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(students)
+a=sum(grades[0])/len(grades[0])
+b=sum(grades[1])/len(grades[1])
+c=sum(grades[2])/len(grades[2])
+d=sum(grades[3])/len(grades[3])
+e=sum(grades[4])/len(grades[4])
+st=tuple(sorted(students))
+a1=st[0]
+print(a1)
+b1=st[1]
+print(b1)
+c1=st[2]
+print(c1)
+d1=st[3]
+print(d1)
+e1=st[4]
+print(e1)
+dict={a1:a,b1:b,c1:c,d1:d,e1:e}
+print(dict)
